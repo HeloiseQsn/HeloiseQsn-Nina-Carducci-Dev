f9e3b24c27e6b9858ddb0cf06cd6489489060959
Héloïse <heloise.lafontaine@gmail.com>
2024-10-16 18:01:35 +0200

    Corrections html : script en fin de body, ajout lang=fr au html, ajout title dans le head, ajout alt sur img, revue des titres, correction nav header, revue affichage des ancres, revue indentation

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,3 +13,20 @@ $(document).ready(function() {
         tagsPosition: 'top'
     });
 });
+
+// Code pour ajuster affichage des ancres
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 100; // Ajuste cette valeur selon la hauteur de ton header
+
+        window.scrollTo({
+            top: targetElement.offsetTop - offset,
+            behavior: 'smooth'
+        });
+    });
+});
